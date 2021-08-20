411c0e2a6f9ab18f7b4f2ec3f9eb97eedc133c17
F Hersi <33755306+fhersi@users.noreply.github.com>
2021-08-20 10:10:28 +0100

    add - mongo injection protection
    added express-mongo-sanitize middleware which removes/ sanitises malicious chars in query string

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 const path = require('path');
 const ExpressError = require('./utils/ExpressError');
@@ -61,6 +63,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(mongoSanitize());
+
 
 
 app.use(express.static(path.join(__dirname, 'public')))
